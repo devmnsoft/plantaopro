539ead6c4c13515c37cdcc262cfa775d032a2af5
marcelo <marcelo.nascimento@cellent.com.br>
2026-05-20 12:25:01 -0300

    correção

--- backend/PlantaoPro.Api/Controllers/UsuariosController.cs
+++ backend/PlantaoPro.Api/Controllers/UsuariosController.cs
@@ -2,6 +2,7 @@ using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using PlantaoPro.Api.Data;
 using PlantaoPro.Api.Models;
 using System.Security.Claims;
 

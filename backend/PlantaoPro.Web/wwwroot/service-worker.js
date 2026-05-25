e02f6dc694cdd199e9aa7ad42f657cd2f7cc2ca6
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-05-25 12:33:42 -0300

    feat: iniciar base SaaS premium com PWA e SQL incremental

---
+++ backend/PlantaoPro.Web/wwwroot/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'plantaopro-static-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/css/custom.css',
+  '/js/site.js',
+  '/branding/logo-plantaopro.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)).catch(() => Promise.resolve())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => cached))
+  );
+});
